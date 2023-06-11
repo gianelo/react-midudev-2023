15ab49c1068deee2a41d6697b1bac813287d4051
Gian Barboza <gbarboza@hablax.com>
2023-06-10 22:25:04 -0500

    Follower Mouse

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,18 +1,61 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
-function App() {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    console.log("Efecto")
-  })
-  
+    console.log('Efecto ', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+      console.log('handleMovie', { clientX, clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // Cleanup
+    // -> Se ejecuta cuando el componente se desmonta
+    // -> Cuando cambian las dependencias, antes de ejecutar el efecto
+    return () => {
+      console.log('Cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
     <>
-      <h3>Proyecto 3</h3>
-      <button>Activar seguir puntero</button>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
     </>
   )
 }
 
-export default App;
+function App () {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
